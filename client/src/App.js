c6051226f12658cf4a9b7d4df987e400feee344d
Kebron Solomon Z <keb2@macbook-pro.lan>
2022-01-27 15:32:36 +0100

    add JSX structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import react from "react"
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
+import memoriesLogo from './img/memoIMG.png'
+import Posts from "./components/Posts/Posts"
+import Form from "./components/Form/form"
 
 function App () {
       return(
-            <div>
-                  <h1>Water</h1>
-            </div>
+          <Container maxWidth='lg'>
+          <AppBar position="static" color="inherit">
+          <Typography variant='h2' align="center" >Memories </Typography>
+          <img src={memoriesLogo}  alt="memories_logo" height='100' width='100'></img>
+          </AppBar>
+          <Grow in>
+            <Container >
+            <Grid container justify='space-between' alignItems="stretch" spacing={3}>
+                <Grid item xs={12} sm={7}>
+                 <Posts/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                 <Form/>
+                </Grid>
+            </Grid>
+            </Container>
+          </Grow>
+          </Container>
       )
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./1Post/1Post.js";
+
+const Posts = ()=>{
+      return(
+            <>
+            <h1>Posts</h1>
+            <Post/>
+            <Post/>
+
+            </>
+      )
+}
+
+export default Posts
